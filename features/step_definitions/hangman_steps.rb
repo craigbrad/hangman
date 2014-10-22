2657a42b3081d43d1d0509f605743816f58493fc
Craig Bradley <craig-bradley@onthebeach.co.uk>
2014-10-22 10:39:56 +0100

    created step definitions template

---
+++ features/step_definitions/hangman_steps.rb
@@ -0,0 +1,67 @@
+Given(/^a correct letter$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the player guesses the letter$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the letter should be revealed in the answer$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^an incorrect letter$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the letter should go into the trash$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the player should lose a life$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the player has one life left$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the player guesses incorrectly$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the game should be over$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^there is only one letter left to guess$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the player guesses the letter correctly$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the game should be won$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a letter has already been guessed correctly$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^the player guesses that letter again$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the player will not lose a life$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a letter has already been guessed incorrectly$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the player will lose a life$/) do
+  pending # express the regexp above with the code you wish you had
+end
