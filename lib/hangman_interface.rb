c1f3a2894cda93a9f5f1ba113243fb17ab25cfe4
Craig Bradley <craig-bradley@onthebeach.co.uk>
2014-10-31 11:39:17 +0000

    fix?

--- lib/hangman_interface.rb
+++ lib/hangman_interface.rb
@@ -63,7 +63,8 @@ post '/slack' do
   @text = params[:text]
   content_type :json
   if slack_game.game == nil
-    { :text => "Please type *hangman newgame* to start", :username => "Hangman HelpBot" }.to_json
+   puts { :text => "Please type *hangman newgame* to start", :username => "Hangman HelpBot" }.to_json
+   { :text => "Please type *hangman newgame* to start", :username => "Hangman HelpBot" }.to_json
   else
     { :text => slack_game.check_command(@text) + "\ntrash: " + slack_game.game.trash.display + "\nlives: " + slack_game.game.lives.number_of_lives.to_s }.to_json
   end
