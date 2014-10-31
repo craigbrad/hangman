c09e87b11546a6cb69362ba7a0ec1cfe65adffe6
Craig Bradley <craig-bradley@onthebeach.co.uk>
2014-10-31 15:27:18 +0000

    fix stuff

--- lib/slack.rb
+++ lib/slack.rb
@@ -14,7 +14,7 @@ class Slack
         message = @game.display.message
         if @game.is_won?
           message = "Congrats, you guessed " + @game.get_answer + " correctly!"
-        elsif @game.lives.number_of_lives == 0
+        elsif @game.is_over?
           message = "Unlucky, the word was " + @game.get_answer
         end
         message 
